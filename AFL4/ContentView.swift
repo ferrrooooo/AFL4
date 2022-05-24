55f3c91a9407789c1aa95101550f6a075c9f0772
ferrrooooo <fputraasmara@student.ciputra.ac.id>
2022-05-24 08:34:05 +0700

    Initial Commit

---
+++ AFL4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AFL4
+//
+//  Created by Ferdy Putra Asmara on 24/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
