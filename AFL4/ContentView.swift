1faff2b80ae6b6b9549db9b3c80f87c029e7fb93
ferrrooooo <fputraasmara@student.ciputra.ac.id>
2022-05-27 15:22:06 +0700

    a

--- AFL4/ContentView.swift
+++ AFL4/ContentView.swift
@@ -9,8 +9,77 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            VStack() {
+                HStack {
+                    Image(systemName: "pin")
+                        .resizable()
+                        .frame(width: 23, height: 42)
+                        .padding(.leading,15)
+                        .padding(.trailing,5)
+                    
+                    VStack {
+                        Text("Tempat Wisata")
+                            .fontWeight(.semibold)
+                            .font(.title)
+                            .padding(.trailing, 134)
+                        Text("Tourist Attraction")
+                            .font(.subheadline)
+                            .multilineTextAlignment(.leading)
+                            .padding(.trailing,200)
+                    }
+                }
+                
+                
+            .background(Color.gray)
+            .cornerRadius(10)
+        }
+            .padding(.bottom,30)
+            
+            VStack() {
+                HStack {
+                    Image(systemName: "heart")
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                        .padding(.leading,10)
+                    VStack {
+                        Text("Favorite")
+                            .fontWeight(.semibold)
+                            .font(.title)
+                            .padding(.trailing, 220)
+                        Text("Tourist Attraction")
+                            .font(.subheadline)
+                            .multilineTextAlignment(.leading)
+                            .padding(.trailing,205)
+                    }
+                }
+            .background(Color.gray)
+            .cornerRadius(10)
+            }
+            .padding(.bottom,30)
+            VStack() {
+                HStack {
+                    Image(systemName: "gearshape")
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                        .padding(.leading,10)
+                    VStack {
+                        Text("Settings")
+                            .fontWeight(.semibold)
+                            .font(.title)
+                            .padding(.trailing, 220)
+                        Text("Tourist Attraction")
+                            .font(.subheadline)
+                            .multilineTextAlignment(.leading)
+                            .padding(.trailing,205)
+                    }
+                }
+            .background(Color.gray)
+            .cornerRadius(10)
+            }
+        }
+                
     }
 }
 
